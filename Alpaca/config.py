35cb39c283e3ba1aeab426a8ce37099977dd7e41
F10JM <146994190+F10JM@users.noreply.github.com>
2023-11-10 19:56:36 +0100

    Delete Alpaca directory

--- Alpaca/config.py
+++
@@ -1,8 +0,0 @@
-ENDPOINT_URL="https://paper-api.alpaca.markets"
-API_KEY="PKM83SCG7GK5OHPJKEEM"
-SECRET_KEY="EWs4qDxigKmPakY97ccqPdS3GPLhnxd8F017Hrid"
-HEADERS = {
-    'APCA-API-KEY-ID': API_KEY,
-    'APCA-API-SECRET-KEY': SECRET_KEY
-}
-BARS_URL = 'https://data.alpaca.markets/v2'
